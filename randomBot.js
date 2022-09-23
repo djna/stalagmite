f34e5938c7194600c0fc207c85a02d7a01550489
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-09-23 10:00:47 +0100

    start

---
+++ randomBot.js
@@ -0,0 +1,46 @@
+class Bot {
+   
+    constructor() {
+        this.instance =  "R:";
+        console.log("creating ", this.instance);
+        this.myDynaCount = 100;
+        this.oppDynaCount = 100;
+        this.validMoves = ['W', 'R', 'P', 'S', 'D'];
+    }
+    log(...dataToLog){
+         console.log(this.instance, ...dataToLog);
+    }
+
+    makeMove(gamestate) {
+        let rounds = gamestate.rounds;
+     
+        if ( rounds.length > 0 ){
+            let lastMove = rounds[rounds.length - 1];
+          
+            if (this.oppDynaCount > 0 && lastMove.p2 === 'D') {
+                this.oppDynaCount--;
+                //this.log("opponent oppDynaCount", this.oppDynaCount);
+                
+                if (this.oppDynaCount <= 0) {
+                    this.validMoves.shift();
+                    this.log("opponent dynamite gone, ", this.validMoves);
+                }
+            }
+        }
+
+        const index = Math.floor(Math.random() * this.validMoves.length);
+      
+        const move = this.validMoves[index]
+        if (move === 'D') {
+            this.myDynaCount--;
+            if (this.myDynaCount == 0) {
+                this.log("dynagone");
+                this.validMoves.pop();
+            }
+        }
+        //this.log("playing", move);
+        return move;
+    }
+}
+
+module.exports = new Bot();
